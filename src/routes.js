5cc1086bf99b82bdd78c488410eaf56094aacfc5
matheus bueno <matheusbueno1063@gmail.com>
2019-08-14 07:43:22 -0300

    host do github

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './containers/Home';
 import Challenge from './containers/Challenge';
 
 const Routes = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/desafio" component={Challenge} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Routes;
